1ea8326971892f5653f29a23de4cf8369a8e933e
Rajendra <natuvarajendra@gmail.com>
2025-05-14 14:52:43 -0400

    created useGames hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,26 +1,10 @@
+import useGames from "@/hooks/useGames";
 import apiClient from "@/services/api-client";
 import React, { useEffect, useState } from "react";
 
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface GamesResponse {
-  count: number;
-  results: Game[];
-}
-
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
+  const { games, error } = useGames();
 
-  useEffect(() => {
-    apiClient
-      .get<GamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
   return (
     <>
       {error && <p>{error}</p>}
