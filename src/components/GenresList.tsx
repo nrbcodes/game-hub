bd7a39afc4ada772e268a6dcc86da7d49e493a00
Rajendra <natuvarajendra@gmail.com>
2025-05-18 14:24:35 -0400

    cleaned Genre list

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,6 +1,14 @@
 import useGenres, { Genre } from "@/hooks/useGenres";
 import getCroppedImageUrl from "@/services/image-url";
-import { Button, HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Heading,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Text,
+} from "@chakra-ui/react";
 import GenreListSkeleton from "./GenreListSkeleton";
 import { GameQuery } from "@/App";
 
@@ -12,32 +20,40 @@ interface Props {
 const GenresList = ({ onSelectGenre, gameQuery }: Props) => {
   const { data, isLoading } = useGenres();
   return (
-    <List>
-      {isLoading &&
-        Array.from({ length: 20 }).map((_, index) => (
-          <GenreListSkeleton key={index} />
-        ))}
+    <>
+      <Heading fontSize="2xl" marginBottom={5}>
+        Genres
+      </Heading>
+      <List>
+        {isLoading &&
+          Array.from({ length: 20 }).map((_, index) => (
+            <GenreListSkeleton key={index} />
+          ))}
 
-      {data.map((g) => (
-        <ListItem key={g.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(g.image_background)}
-            />
-            <Button
-              fontWeight={gameQuery.genre?.id === g.id ? "bold" : "normal"}
-              fontSize="lg"
-              variant="link"
-              onClick={() => onSelectGenre(g)}
-            >
-              {g.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+        {data.map((g) => (
+          <ListItem key={g.id} paddingY="5px">
+            <HStack>
+              <Image
+                objectFit="cover"
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImageUrl(g.image_background)}
+              />
+              <Button
+                fontWeight={gameQuery.genre?.id === g.id ? "bold" : "normal"}
+                whiteSpace="normal"
+                fontSize="lg"
+                textAlign="left"
+                variant="link"
+                onClick={() => onSelectGenre(g)}
+              >
+                {g.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
